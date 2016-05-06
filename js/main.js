62ad98c18bf00d2364c26a12aec88ff61dc2954f
thewinger <winger.87@gmail.com>
2016-05-06 14:19:07 +0200

    mod links to point correctly

--- js/main.js
+++ js/main.js
@@ -42,5 +42,12 @@ function toggleMenu() {
 
 
 
-//=include contact-form.js
+var contactform =  document.getElementById('form-contact');
+var email = btoa('winger.87');
+var server = btoa('gmail.com');
+var base_url = '//formspree.io/';
+var action =  base_url + atob(email) + '@' + atob(server); 
+contactform.setAttribute('action', action); 
+
+
 });
