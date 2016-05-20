1c1af2b24c5fb82399fa55ca3c414116deb94faf
thewinger <winger.87@gmail.com>
2016-05-20 09:51:36 +0200

    added svg icons + menu-close

--- js/main.js
+++ js/main.js
@@ -20,17 +20,20 @@ window.addEventListener('load', function() {
 // MENU-TOGGLE
 var nav = document.getElementById('js-nav-mobile'); 
 var button = document.getElementById('js-menu-toggle');
+var menu = document.getElementsByClassName('i-menu')[0];
 button.classList.add('menu-is-hided');
 button.addEventListener('click', toggleMenu, false);
-//button.addEventListener('touchstart', toggleMenu, false);
 
 function toggleMenu() {
   if (containsClass(button, 'menu-is-hided')) {
     toggleClass(button,'menu-is-hided');
     toggleClass(nav, 'nav-is-displayed');
+    addClass(menu, 'i-menu-close');
+    console.log(menu + "close");
   } else {
     toggleClass(button, 'menu-is-hided');
     toggleClass(nav, 'nav-is-displayed');
+    removeClass(menu, 'i-menu-close');
   } 
   function show(elements) {
     elements.forEach(function(el) { el.style.display = "block"; }); 
